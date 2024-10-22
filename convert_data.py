e010e6f27f8e11650844c1c0d4c16e4c9fbfb389
Oliver Lylloff <ollyl@dtu.dk>
2024-10-22 12:46:10 +0200

    update data

--- convert_data.py
+++ convert_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import geopandas
-years = [f'Y{x}' for x in range(1977,2024)]
-df = pd.read_excel('anlaeg.xlsx',skiprows=13,header=0,usecols='A:BI,BU',
+years = [f'Y{x}' for x in range(1977,2025)]
+df = pd.read_excel('anlaeg.xlsx',skiprows=13,header=0,usecols='A:BJ,BS',
     names = ['id', 'date_connect','date_decom', 'capacity_kW', 'rotor_diam_m',
        'hub_height_m', 'manufacturer', 'type', 'auth',
        'location', 'district', 'district_no',
@@ -10,4 +10,4 @@ df = pd.read_excel('anlaeg.xlsx',skiprows=13,header=0,usecols='A:BI,BU',
 
 points = geopandas.points_from_xy(x=df.X_UTM_32_ETRS89, y=df.Y_UTM_32_ETRS89,crs="EPSG:25832")
 gdf = geopandas.GeoDataFrame(df, geometry=points).to_crs("EPSG:4326")
-gdf.to_file('wt_2024.json', driver='GeoJSON')
+gdf.to_file('wt_2024oct.json', driver='GeoJSON')
