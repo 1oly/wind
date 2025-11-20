6ff7f8d4e6e9c491aedd85d632594834d76ccc86
Oliver Lylloff <ollyl@dtu.dk>
2025-11-20 08:57:57 +0100

    update with more heights

--- script/forecastedr_script_DINI.py
+++ script/forecastedr_script_DINI.py
@@ -7,6 +7,8 @@ import json
 import pytz
 from dmi_forecast_edr import DMIForecastEDRClient, Collection
 
+HEIGHTS = ['10m', '50m', '100m', '150m', '250m', '350m', '450m']
+
 try:
     DMI_API_KEY = os.environ['DMI_API_KEY']
 except KeyError:
@@ -17,38 +19,42 @@ client = DMIForecastEDRClient(api_key=DMI_API_KEY)
 dtnow = datetime.now(pytz.timezone('Europe/Copenhagen')).replace(tzinfo=None)
 
 forecast = client.get_forecast(
-    collection = Collection.HarmonieDiniSf,
-    parameter = ['wind-speed','wind-dir'],
-    crs = 'crs84',
-    to_time = dtnow,
-    f = 'GeoJSON',
-    coords = [3.00,52.00,20.00,65.00]
+    collection=Collection.HarmonieDiniSf,
+    parameter=[f'wind-speed-{h}' for h in HEIGHTS] + [f'wind-dir-{h}' for h in HEIGHTS],
+    crs='crs84',
+    to_time=dtnow,
+    f='GeoJSON',
+    coords=[3.00, 52.00, 20.00, 65.00]
 )
 
 geo = []
 step = []
-winddir = []
-windspeed = []
+winddir_data = {height: [] for height in HEIGHTS}
+windspeed_data = {height: [] for height in HEIGHTS}
 for feature in forecast:
     geo.append(feature['geometry']['coordinates'])
     step.append(feature['properties']['step'])
-    winddir.append(feature['properties']['wind-dir'])
-    windspeed.append(feature['properties']['wind-speed'])
+    props = feature['properties']
+    for height in HEIGHTS:
+        winddir_data[height].append(props[f'wind-dir-{height}'])
+        windspeed_data[height].append(props[f'wind-speed-{height}'])
 
 geo = np.asarray(geo)
 step = np.asarray(step)
-winddir = np.asarray(winddir)
-windspeed = np.asarray(windspeed)
+winddir_data = {height: np.asarray(values) for height, values in winddir_data.items()}
+windspeed_data = {height: np.asarray(values) for height, values in windspeed_data.items()}
 
 ind = np.lexsort((geo[:, 1], geo[:, 0]))
+geo_sorted = geo[ind]
+step_sorted = step[ind]
+ref_time = step_sorted[0] if step_sorted.size else None
 
-for name,winddir,windspeed in zip(['10m'],[winddir],[windspeed]):
-    geo = geo[ind]
-    winddir = winddir[ind]
-    windspeed = windspeed[ind]
+for height in HEIGHTS:
+    winddir = winddir_data[height][ind]
+    windspeed = windspeed_data[height][ind]
     u_vector = []
     v_vector = []
-    for i in range(geo.shape[0]):
+    for i in range(geo_sorted.shape[0]):
         theta = 270 - winddir[i]
         if theta < 0:
           theta += 360
@@ -59,15 +65,15 @@ for name,winddir,windspeed in zip(['10m'],[winddir],[windspeed]):
         v_vector.append(v)
 
     # grid interpolation on u and v vectors and option for down-sampling
-    loni = np.arange(np.amin(geo[:,0]),np.amax(geo[:,0]),0.5)
-    lati = np.arange(np.amax(geo[:,1]),np.amin(geo[:,1]),-0.25)
-    loni,lati = np.meshgrid(loni,lati)
-    ui = griddata((geo[:,0],geo[:,1]),u_vector,(loni,lati),method='nearest')
-    vi = griddata((geo[:,0],geo[:,1]),v_vector,(loni,lati),method='nearest')
+    loni = np.arange(np.amin(geo_sorted[:, 0]), np.amax(geo_sorted[:, 0]), 0.5)
+    lati = np.arange(np.amax(geo_sorted[:, 1]), np.amin(geo_sorted[:, 1]), -0.25)
+    loni, lati = np.meshgrid(loni, lati)
+    ui = griddata((geo_sorted[:, 0], geo_sorted[:, 1]), u_vector, (loni, lati), method='nearest')
+    vi = griddata((geo_sorted[:, 0], geo_sorted[:, 1]), v_vector, (loni, lati), method='nearest')
 
-    bounds = {'Min lon': np.amin(loni), 'Max lon': np.amax(loni),'Min lat': np.amin(lati), 'Max lat': np.amax(lati)}
+    bounds = {'Min lon': np.amin(loni), 'Max lon': np.amax(loni), 'Min lat': np.amin(lati), 'Max lat': np.amax(lati)}
 
-    lat_ny,lon_nx = loni.shape
+    lat_ny, lon_nx = loni.shape
 
     head_u = {'parameterCategory': 2,
               'parameterNumber': 2,
@@ -77,7 +83,7 @@ for name,winddir,windspeed in zip(['10m'],[winddir],[windspeed]):
               'dy': abs((bounds['Min lat'] - bounds['Max lat']) / (lat_ny - 1)),
               'nx': lon_nx,  # lon W-E
               'ny': lat_ny,  # lat S-N
-              'refTime': step[0]}
+              'refTime': ref_time}
     head_v = head_u.copy()
     head_v['parameterNumber'] = 3
     out_data = [{'header': head_u,
@@ -85,5 +91,5 @@ for name,winddir,windspeed in zip(['10m'],[winddir],[windspeed]):
                      {'header': head_v,
                       'data': list(vi.flatten())}]
 
-    with open("wind{}.json".format(name), 'w') as f:
+    with open("wind{}.json".format(height), 'w') as f:
         json.dump(out_data, f)
